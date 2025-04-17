0afaada4baf2fe52e3ca2d0c5566dd9837cb94e2
luangaldinodev <luangaldinodev@gmail.com>
2025-04-16 21:29:37 -0300

    feat: template de email e envio ok

--- app/functions/sendEmail.php
+++ app/functions/sendEmail.php
@@ -7,6 +7,8 @@ use PHPMailer\PHPMailer\Exception;
 require '../vendor/autoload.php';
 require 'keys.php';
 
+$nome = 'Luan Galdino';
+$bodyMensage = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus natus aspernatur sequi deserunt recusandae voluptatem eum ad ab eveniet ipsam, aliquam blanditiis reiciendis tempora aut commodi delectus quae nam dicta temporibus quisquam amet beatae animi. Repellendus officia vel pariatur laboriosam necessitatibus, doloremque nam dolorem cupiditate, vero aliquid tenetur optio magni fugit omnis veniam cum placeat qui non quos deleniti dignissimos odit! Modi officia quae minima veritatis facilis architecto illo sit exercitationem quisquam blanditiis quod iusto beatae deleniti error dolore eligendi eos, distinctio dolor explicabo consequatur nihil! Pariatur, cum quibusdam eius porro omnis labore voluptatibus facere neque. Magni corrupti officiis qui!';
 
 $mail = new PHPMailer(true);
 
@@ -26,16 +28,16 @@ try {
     $mail->setFrom($emailSend, 'Casa da Limpeza Ubatuba');
     $mail->addAddress('luangaldinodev@gmail.com', 'teste');
     // $mail->addEmbeddedImage('../../src/img/Logo-HDF.jpeg', 'logo_cid');
-    // $emailTemplate = file_get_contents('../../src/html/email_template.html');
-    // $emailTemplate = str_replace(
-        // ['[Nome]'],
-        // [$first_name],
-        // $emailTemplate
-    // );
+    $emailTemplate = file_get_contents('templates_email/template_01.html');
+    $emailTemplate = str_replace(
+        ['[nome]', '[mensagem]'],
+        [$nome, $bodyMensage],
+        $emailTemplate
+    );
 
     $mail->isHTML(true);
     $mail->Subject = 'teste de email';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Body    = $emailTemplate;
 
     $mail->send();
     // return "enviado com sucesso";
